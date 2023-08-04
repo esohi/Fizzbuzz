88a1e013749ea38dc5d5c0ce82b447b76fd66020
esohi <esohi@proton.me>
2023-08-04 16:56:02 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let i = 1;
+while (i <= 100) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+  i++;
+}
+
+
+/*
+Another option:
+
+function fizzBuzz(num) {
+    for (let i = 1; i <= num; i++)
+    {
+      if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+      else if (i % 3 === 0) console.log("Fizz");
+      else if (i % 5 === 0) console.log("Buzz");
+      else console.log(i);
+    }
+  }
+  fizzBuzz(100);
+  */
